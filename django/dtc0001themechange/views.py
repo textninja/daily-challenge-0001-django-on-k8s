2a5e455bbd0e632a395d5690524f9d59af61dca2
Joe Taylor <joe@textninja.net>
2021-12-31 00:22:49 -0800

    Made background color configurable

--- django/dtc0001themechange/views.py
+++ django/dtc0001themechange/views.py
@@ -1,5 +1,8 @@
+import os
 from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'dtc0001themechange/index.html')
+    return render(request, 'dtc0001themechange/index.html', {
+        "bg_color":  os.getenv("BG_COLOR", None)
+    })
